037ba1e1c914733ecb1ca5ada8622a2cb447afee
Krishna Devidas Sangale <krishna.sangale@waiin.com>
2024-01-26 20:24:01 +0530

    List page loading setup

---
+++ MoviesKatta/ViewControls/VideoCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesKatta.ViewControls;
+
+public partial class VideoCell : ContentView
+{
+    public VideoCell()
+    {
+        InitializeComponent();
+    }
+}
